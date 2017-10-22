a36c905ed1d32c8387707456e62e333f961a18ed
ArmandoBeltrán <armaabg@gmail.com>
2017-10-21 22:17:28 -0600

    Arranque de la configuracion inicial del proyecto front

---
+++ Front/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render (){
+    return(
+      <div>
+        <h1>Easy Money</h1>
+        <input type="text" placeholder="Usuario"/><br/>
+        <input type="pass" placeholder="Contraseña"/>
+      </div>
+    )
+  }
+}
