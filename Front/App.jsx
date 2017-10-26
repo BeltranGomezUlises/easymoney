b6a4047bf62b3aeeddcd5d9d617d3dcbbc073e50
ArmandoBeltrán <armaabg@gmail.com>
2017-10-26 00:09:26 -0600

    sidebar
    se implemento un sidebar para la navegación de los modulos

--- Front/App.jsx
+++ Front/App.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import { Button,Segment,Divider } from 'semantic-ui-react'
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Button, Container, Divider,  Dropdown,  Header,  Message,  Segment,  Menu, Icon, Sidebar, Image } from 'semantic-ui-react';
 
-export default class App extends React.Component {
-  render (){
-    return(
-      <div className="ui center aligned segment" style={{width: "50%", aling: "center"}}>
-        <h1>Easy Money</h1>
-        <Segment>
-          <Divider section/>
-          <input type="text" placeholder="Usuario"/><br/>
-          <input type="pass" placeholder="Contraseña"/>
-          <Button>boton</Button>
-        </Segment>
-      </div>
-    )
+import Clientes from './app_modules/Clientes.js';
+import Cobradores from './app_modules/Cobradores.js';
+import Prestamos from './app_modules/Prestamos.js';
+
+var mystyle={
+  'height' : '700px'
+}
+
+const App = () => (
+    <Router>
+        <MainContainer></MainContainer>
+    </Router>
+)
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {menuVisible: false};
+  }
+
+  render() {
+
+    return <div>
+      <Menu secondary attached="top">
+        <Menu.Item onClick={() => this.setState({ menuVisible: !this.state.menuVisible })} >
+          <Icon name="sidebar" />Menu
+        </Menu.Item>
+      </Menu>
+    <Sidebar.Pushable as={Segment} attached="bottom" >
+      <Sidebar width='thin' as={Menu} animation="uncover" visible={this.state.menuVisible} icon="labeled" vertical inline inverted>
+        <Menu.Item as={Link} to='/clientes'><Icon name="" />Clientes</Menu.Item>
+        <Menu.Item as={Link} to='/cobradores'><Icon name="" />Cobradores</Menu.Item>
+        <Menu.Item as={Link} to='/prestamos'><Icon name="" />Prestamos</Menu.Item>
+      </Sidebar>
+       <Sidebar.Pusher >
+            <div style={mystyle}>
+
+              <Route path="/clientes" component={Clientes}/>
+              <Route path="/cobradores" component={Cobradores}/>
+              <Route path="/prestamos" component={Prestamos}/>
+
+            </div>
+       </Sidebar.Pusher>
+    </Sidebar.Pushable>
+    </div>
   }
 }
+export default App
