9d3d0930f57569b4c4a3f9c7e6c1689706be33a1
ulises <beltrangomezulises@gmail.com>
2017-10-29 16:51:01 -0700

    fix

--- Front/app_modules/Clientes/Componentes/ClienteCard.jsx
+++ Front/app_modules/Clientes/Componentes/ClienteCard.jsx
@@ -5,13 +5,11 @@ import { Card , Icon, Input, Button} from 'semantic-ui-react';
 export default class ClienteCard extends React.Component{
 
   constructor(props) {
-    super(props);    
+    super(props);
+    this.state = {open:false};
   }
-  toggleModal = () =>{
-    this.setState({open: !this.state.open});
-  }
-  render(){
 
+  render(){
     return(
         <Card>
           <Card.Content>
@@ -26,9 +24,8 @@ export default class ClienteCard extends React.Component{
              </Card.Description>
           </Card.Content>
           <Card.Content extra>
-             <div className='ui two buttons'>
-               <Button >Tiny</Button>
-               <Button basic color='blue' onClick={.show()}>Editar</Button>
+             <div className='ui two buttons'>               
+               <Button basic color='blue'>Editar</Button>
                <Button basic color='red'>Eliminar</Button>
              </div>
           </Card.Content>
