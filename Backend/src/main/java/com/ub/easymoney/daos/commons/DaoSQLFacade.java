57dc48974a81d2e0c7a386d8f64a7944b0e8d53a
Ulises Beltran <beltrangomezulises@gmail.com>
2018-04-29 09:53:10 -0600

    backupd

--- Backend/src/main/java/com/ub/easymoney/daos/commons/DaoSQLFacade.java
+++ Backend/src/main/java/com/ub/easymoney/daos/commons/DaoSQLFacade.java
@@ -18,7 +18,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import org.jinq.jpa.JPAJinqStream;
 import org.jinq.jpa.JPAQueryLogger;
 import org.jinq.jpa.JinqJPAStreamProvider;
